1a90581ca6403e5279699805b8b6aab6a897eee1
zhangweiqiang666 <972618478@qq.com>
2019-08-20 18:40:28 +0800

    项目初始化

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    Home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style scoped >
+</style>
---
+++ src/pages/list/List.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    List
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  data () {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style scoped >
+</style>
