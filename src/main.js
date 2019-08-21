b397a1d6fab6a3005c2db749a8971c6399a9dc8d
zhangweiqiang666 <972618478@qq.com>
2019-08-21 10:54:55 +0800

    轮播

--- src/main.js
+++ src/main.js
@@ -2,13 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+// 解决三秒延迟
 import fastClick from 'fastclick'
+// 轮播插件
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 import router from './router'
 import 'style/reset.css'
 import 'style/border.css'
 import 'style/iconfont.css'
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
